52c50e3c6278d26aaa8e77b29148f34aa937d2b3
Obyedullahilmamun <obyedullahilmamun@gmail.com>
2024-07-01 02:32:41 +0600

    Model Modified

--- src/model.py
+++ src/model.py
@@ -19,7 +19,7 @@ class DeepPunctuation(nn.Module):
         else:
             hidden_size = lstm_dim
         self.lstm = nn.LSTM(input_size=bert_dim, hidden_size=hidden_size, num_layers=1, bidirectional=True)
-        self.linear = nn.Linear(in_features=hidden_size*2, out_features=len(punctuation_dict))
+        self.linear = nn.Linear(in_features=hidden_size*2, out_features=5)
 
     def forward(self, x, attn_masks):
         if len(x.shape) == 1:
